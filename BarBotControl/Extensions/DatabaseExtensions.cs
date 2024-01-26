e5bd1f4204bff6f41ec4fb527cbd7b5e227d0b4c
JSON Alexander <61407072+jasonalexander-ja@users.noreply.github.com>
2024-01-26 15:50:06 +0000

    Added flag for disabling database console logging.

--- BarBotControl/Extensions/DatabaseExtensions.cs
+++ BarBotControl/Extensions/DatabaseExtensions.cs
@@ -8,15 +8,21 @@ public static class DatabaseExtensions
     public static WebApplicationBuilder AddDatabase(this WebApplicationBuilder builder)
     {
         var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        var logToConsole = builder.Configuration.GetSection("EnableConsoleDBLogging")
+            .Get<bool>();
 
         var serverVersion = new MySqlServerVersion(new Version(11, 2, 2));
 
         builder.Services.AddDbContext<AppDbContext>(options =>
-            options.UseMySql(connectionString, serverVersion)
-                .LogTo(Console.WriteLine, LogLevel.Information)
-                .EnableSensitiveDataLogging()
-                .EnableDetailedErrors()
-        );
+        {
+            var dbBuilder = options.UseMySql(connectionString, serverVersion);
+            if (logToConsole)
+            {
+                dbBuilder.LogTo(Console.WriteLine, LogLevel.Information)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors();
+            }
+        });
         return builder;
     }
 }
